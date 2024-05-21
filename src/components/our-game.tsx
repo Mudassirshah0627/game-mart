aa44707af13bde36840571f4d6e73a1fe3165984
Mudassirshah0627 <mudassirshah0627@gmail.com>
2024-05-20 23:14:15 -0700

    responsive

--- src/components/our-game.tsx
+++ src/components/our-game.tsx
@@ -8,30 +8,18 @@ const OurGame = memo(function OurGame() {
   return (
     <div className="bg-[#2E2D2E] py-12 mt-6 rounded-3xl ">
       <div className="px-12">
-        <motion.h1
-          initial={{ opacity: 0, scale: 0.5, x: -170 }}
-          whileInView={{ opacity: 1, scale: 1, x: 0 }}
-          transition={{ duration: 1.2 }}
-          className="text-3xl font-bold  "
-        >
-          Our Games
-        </motion.h1>
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5, y: 70 }}
-          whileInView={{ opacity: 1, scale: 1, y: 0 }}
-          transition={{ duration: 0.8 }}
-          className="py-4 text-lg text-white pb-4"
-        >
+        <h1 className="text-3xl font-bold text-white pt-12 ">Our Games</h1>
+        <p className="py-4 text-lg text-white">
           Step into a world where art meets blockchain technology. Our exclusive
           collection of non-fungible tokens (NFTs) showcases some of the most
           unique and stunning works of digital art from talented artists around
           the world. Each NFT is a one-of-a-kind creation, verified on the
           blockchain, and is sure to capture the attention of any collector or
           art enthusiast.
-        </motion.div>
+        </p>
       </div>
-      <div className="md:flex justify-between px-5 pb-32  md:h-screen  md:relative">
-        <motion.div className=" first w-full md:w-[45%] xl:w-1/2  h-[60vh] md:h-[80vh] absolute left-4 xl:left-12  md:px-12 pb-3 bottom-32">
+      <div className="md:flex justify-between px-5 pb-12 h-[140vh] md:h-screen relative">
+        <div className=" first w-full md:w-[45%] xl:w-1/2 h-[60vh] md:h-[95vh]  left-4 xl:left-12  md:px-12 pb-3 relative">
           <div className="h-[13rem] xl:w-[20rem] flex justify-center py-12">
             <Image
               src="/assets/Images/ourgame.png"
@@ -41,7 +29,7 @@ const OurGame = memo(function OurGame() {
               className="h-full w-full"
             />
           </div>
-          <motion.div className="absolute -bottom-4 sm:bottom-4">
+          <div className="absolute -bottom-4 sm:bottom-4">
             <h1 className="text-2xl font-bold text-white    ">Blops</h1>
             <p className="text-white text-sm pr-12 ">
               Blops are a unique and quirky collection of NFTs featuring bizarre
@@ -53,9 +41,9 @@ const OurGame = memo(function OurGame() {
             <button className="py-2 text-xl mt-4 px-12 bg-white text-primary shadow-md rounded-md">
               Play Now
             </button>
-          </motion.div>
-        </motion.div>
-        <motion.div className="my-3 md:my-0 second w-full md:w-[45%] xl:w-1/2 h-[60vh] md:h-[80vh] absolute bottom-32 right-4 xl:right-12">
+          </div>
+        </div>
+        <div className="second w-full md:w-[45%] xl:w-1/2 h-[60vh] mt-6 md:mt-0 md:h-[95vh] absolute right-4 xl:right-12">
           <div className="flex flex-col  float-end w-full lg:w-[80%] px-12">
             <h1 className="text-2xl font-bold pt-6">Pixel Hero</h1>
             <p className="text-white text-sm ">
@@ -67,7 +55,7 @@ const OurGame = memo(function OurGame() {
               Heroes collection comp xel Heroes is a card game NFT project built
               by an early Proton NFT creator. The project has been handed
             </p>
-            <button className="py-2 text-xl mt-4 px-12 bg-primary  shadow-md rounded-md">
+            <button className="py-2 text-xl mt-4 px-12 bg-[#2E2D2E] text-white shadow-md rounded-md">
               Play Now
             </button>
           </div>
@@ -80,7 +68,7 @@ const OurGame = memo(function OurGame() {
               className="h-full"
             />
           </div>
-        </motion.div>
+        </div>
       </div>
     </div>
   );
